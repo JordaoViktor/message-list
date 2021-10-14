a58107bc99e510c62b57d8e36e406dcff8b95753
JordaoViktor <jordaovpinheiro@gmail.com>
2021-10-14 16:57:57 -0300

    :construction: trying to fix the json server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { Platform } from 'react-native';
+
+const host = Platform.OS === 'ios' ? 'localhost' :  '10.0.2.2';
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333/",
+  baseURL: `http://${host}:3333`
 });
 
 export { api };
